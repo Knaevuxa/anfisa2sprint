abbda23f5a202feeb67e82c057107c399081d688
Стас Басов <stas.basov@whatever.com>
2024-06-10 20:55:15 +0300

    privet

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -1,11 +1,20 @@
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-oy%f52n92s=%3&b3dh&h)f(b4ag^_z*&3w+ooo!sy-g=449bwn'
 
 DEBUG = True
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 ALLOWED_HOSTS = []
 
@@ -20,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -30,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'anfisa_for_friends.urls'
@@ -79,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Q
 
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Заключаем вызов методов в скобки
+    # (это стандартный способ переноса длинных строк в Python);
+    # каждый вызов пишем с новой строки, так проще читать код:
+    ice_cream_list = IceCream.objects.values(
+            'id', 'title', 'description'
+        # Верни только те объекты, у которых в поле is_on_main указано True:
+        ).filter(Q(is_published=True) & (Q(is_on_main=False) | Q(title__contains='пломбир'))).order_by('title')[1:4] 
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context) 
