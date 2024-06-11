6e3c1522f8d2ca80d7548f6225216a5f05e5c0ed
Николай Татаров <turbo51@yandex.ru>
2024-06-12 01:09:02 +0300

    lastupdate

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.db.models import Q
-
 from ice_cream.models import IceCream
+
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Заключаем вызов методов в скобки
-    # (это стандартный способ переноса длинных строк в Python);
-    # каждый вызов пишем с новой строки, так проще читать код:
+    # Запрашиваем нужные поля из базы данных:
     ice_cream_list = IceCream.objects.values(
-            'id', 'title', 'description'
-        # Верни только те объекты, у которых в поле is_on_main указано True:
-        ).filter(Q(is_published=True) & (Q(is_on_main=False) | Q(title__contains='пломбир'))).order_by('title')[1:4] 
+        'id', 'title', 'price', 'description'
+    ).filter(
+        # Проверяем, что
+        is_published=True,  # Сорт разрешён к публикации;
+        is_on_main=True,  # Сорт разрешён к публикации на главной странице;
+        category__is_published=True  # Категория разрешена к публикации.
+    )
+
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    return render(request, template_name, context) 
+    return render(request, template_name, context)
--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -5,11 +5,8 @@ from ice_cream.models import IceCream
 
 def ice_cream_detail(request, pk):
     template_name = 'ice_cream/detail.html'
-    # Вызываем .get() и в его параметрах указываем условия фильтрации:
     ice_cream = get_object_or_404(
-        IceCream.objects.values(
-            'title', 'description'
-        ).filter(is_published=True),
+        IceCream.objects.filter(is_published=True, category__is_published=True),
         pk=pk
     )
     context = {
@@ -20,5 +17,10 @@ def ice_cream_detail(request, pk):
 
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
-    context = {}
-    return render(request, template, context)
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+        is_published=True,
+        category__is_published=True
+    ).order_by('category')
+
+    context = {'ice_cream_list': ice_cream_list}
+    return render(request, template, context) 
